b693482022c8b884a95659fbb5a5a81524e6332f
Danny van der Meulen <dmeulen@ebay.com>
2017-12-12 16:37:44 +0100

    Start up services

--- rootfs/init/run.sh
+++ rootfs/init/run.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-#
-# run this
+#!/usr/bin/env sh
+
+busybox syslogd -n -O /dev/stdout &
+bbchild=$!
+
+consul-template -config /etc/ndocker/postfix.hcl &
+child=$!
+
+trap "kill $bbchild $child" INT TERM
+wait $child
