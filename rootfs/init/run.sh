c02d5899dce120163162b6abb4c851c7732b86a7
Danny van der Meulen <dmeulen@ebay.com>
2017-12-13 17:26:18 +0100

    safer perms on sasl passwd file

--- rootfs/init/run.sh
+++ rootfs/init/run.sh
@@ -2,6 +2,8 @@
 
 if [ $POSTFIX_CLIENT_SASL_ENABLED == "true" ]; then
   echo "$SASL_GATEWAY $SASL_USERNAME:$SASL_PASSWD" > /etc/postfix/sasl_passwd
+  chmod 640 /etc/postfix/sasl_passwd
+  chown postfix:postfix /etc/postfix/sasl_passwd
   postmap /etc/postfix/sasl_passwd
 fi
 
