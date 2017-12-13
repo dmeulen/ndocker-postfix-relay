17ccaed82e034e9b8178cd079ccb8046123cd57b
Danny van der Meulen <dmeulen@ebay.com>
2017-12-13 17:30:26 +0100

    should be owned by root

--- rootfs/init/run.sh
+++ rootfs/init/run.sh
@@ -2,9 +2,8 @@
 
 if [ $POSTFIX_CLIENT_SASL_ENABLED == "true" ]; then
   echo "$SASL_GATEWAY $SASL_USERNAME:$SASL_PASSWD" > /etc/postfix/sasl_passwd
-  chmod 640 /etc/postfix/sasl_passwd
-  chown postfix:postfix /etc/postfix/sasl_passwd
   postmap /etc/postfix/sasl_passwd
+  chmod 640 /etc/postfix/sasl_passwd*
 fi
 
 if test $NOMAD_PORT_smtp; then
